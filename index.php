5ef4e47876fb5c56fb2fb16e929b4a0ec8f0da0a
anastasiia-matushenko <muzaam07@gmail.com>
2024-05-28 00:25:43 +0300

    add hw-3

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-echo 'Homework 2!';
+require_once './classes/Color.php';
+
+
+$random = Color::random();
+echo "{$random->getRed()}-{$random->getGreen()}-{$random->getBlue()}\n";
+
+$color = new Color(250, 250, 250);
+$mixedColor = $color->mix(new Color(100, 100, 100));
+echo "{$mixedColor->getRed()}-{$mixedColor->getGreen()}-{$mixedColor->getBlue()}";
+
