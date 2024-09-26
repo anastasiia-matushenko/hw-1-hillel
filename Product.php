f9c676d84ac57ca8da7197bfd514f90e7db69552
anastasiia-matushenko <77169504+anastasiia-matushenko@users.noreply.github.com>
2024-09-26 21:06:31 +0200

    add hw_7

---
+++ Product.php
@@ -0,0 +1,20 @@
+<?php
+
+class Product
+{
+    public function get($name) {}
+    public function set($name, $value) {}
+}
+
+class ProductProcessing
+{
+    public function save() {}
+    public function update() {}
+    public function delete() {}
+}
+
+class ProductOutput
+{
+    public function show() {}
+    public function print() {}
+}
